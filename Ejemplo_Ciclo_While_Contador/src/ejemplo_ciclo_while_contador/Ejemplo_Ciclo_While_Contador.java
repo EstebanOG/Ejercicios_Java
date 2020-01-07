3bd036ca5bd0b027ca4f48032660f74c79f23655
Esteban <juanesteban199911 gmail.com>
2020-01-07 16:44:42 -0500

    Se agrega ejemplo ciclo while

---
+++ Ejemplo_Ciclo_While_Contador/src/ejemplo_ciclo_while_contador/Ejemplo_Ciclo_While_Contador.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo_ciclo_while_contador;
+
+/**
+ *
+ * @author juane
+ */
+public class Ejemplo_Ciclo_While_Contador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int cont = 0;
+        int maximo;
+        maximo = 10;
+        
+        while(cont < maximo){
+            System.out.println("Contador: "+cont);
+            cont = cont + 1;
+        }
+    }
+    
+}
