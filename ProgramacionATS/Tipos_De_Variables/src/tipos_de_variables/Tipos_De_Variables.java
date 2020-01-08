3ff89dc478312f154d9f510a18a7eb16884aa34e
Esteban <juanesteban199911 gmail.com>
2020-01-08 09:59:50 -0500

    Se agrega ejercicio Tipo_De_Variables

---
+++ ProgramacionATS/Tipos_De_Variables/src/tipos_de_variables/Tipos_De_Variables.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipos_de_variables;
+
+/**
+ *
+ * @author juane
+ */
+public class Tipos_De_Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte entero = 12;
+        System.out.println("Número entero:"+ entero);
+        short entero2 = 12456;
+        System.out.println("Número entero:"+entero2);
+        int entero3 = 124586709;
+        System.out.println("Número entero:"+entero3);
+        long entero4 = 123948550;
+        System.out.println("Número entero:"+entero4);
+        float decimal = 3.45f;
+        System.out.println("Número decimal:"+decimal);
+        double decimal2 = 3.412321;
+        System.out.println("Número decimal:"+decimal2);
+        char caracter = 'a';
+        System.out.println("Caracter: "+caracter);
+        boolean decision = true;
+        System.out.println("La decision es: "+decision);
+        
+    }
+    
+}
