82696e15a51cb49478cae5132426cf45e76e0a4a
Esteban <juanesteban199911 gmail.com>
2020-01-08 09:52:32 -0500

    Se agrega carpeta ProgramacionATS

---
+++ ProgramacionATS/Constantes/src/constantes/Constantes.java
@@ -0,0 +1,15 @@
+
+package constantes;
+
+/**
+ *
+ * @author juane
+ */
+public class Constantes {
+
+    public static void main(String[] args) {
+        final int numero = 10;// Para declarar una constante se debe agregar la palabra final al inicio 
+        System.out.println(numero);
+    }
+    
+}
