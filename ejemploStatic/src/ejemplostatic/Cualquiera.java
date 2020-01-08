092fd559b40a65c6fcc72af9dcfc9e24036946ac
Esteban <juanesteban199911 gmail.com>
2020-01-08 11:19:49 -0500

    ejemplo Static

---
+++ ejemploStatic/src/ejemplostatic/Cualquiera.java
@@ -0,0 +1,16 @@
+package ejemplostatic;
+
+/**
+ *
+ * @author juane
+ */
+public class Cualquiera {
+    private String atributoString;
+    private static int atributoInt;
+    
+    public static void metodoX(){
+        //Hacer algo
+        atributoInt = 2;
+        //Los atributos dentro del metodo deben ser static 
+    }
+}
---
+++ ejemploStatic/src/ejemplostatic/EjemploStatic.java
@@ -0,0 +1,19 @@
+
+package ejemplostatic;
+
+/**
+ *
+ * @author juane
+ */
+public class EjemploStatic {
+
+    public static void main(String[] args) {
+//        Cualquiera c = new Cualquiera();
+//        c.metodoX();
+//        Esto en caso de que la clase no sea STATIC
+
+        Cualquiera.metodoX();
+//      Se accede al metodo desde la clase y no desde un objeto
+    }
+    
+}
